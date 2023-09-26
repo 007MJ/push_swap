5301c8a86e48bb7a26ed4730119113990846e0b5
007MJ <k.mijiza@gmail.com>
2023-09-25 20:27:10 -0400

    keep work on 5 sort

--- src/sort/sort_five.c
+++ src/sort/sort_five.c
@@ -6,19 +6,33 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 17:18:04 by mnshimiy          #+#    #+#             */
-/*   Updated: 2023/09/25 19:36:14 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2023/09/25 20:26:44 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-void	sort_five(t_stack **stack, t_stack **stack_b)
+void	sort_number(t_stack **stack)
 {
 	t_stack	*list;
+	t_stack	*second;
+	t_stack	*three;
+
+	second = search_list(*stack, 2);
+	three = search_list(*stack, 3);
+	list = search_list(*stack, lstsize(*stack));
+	if ((*stack)->nb < list->nb)
+		rotate_a(stack, "ra");
+	if ((*stack)->nb < second->nb && (*stack)->nb < list->nb && (*stack)->nb > three->nb)
+		swap_a_b(*stack, "sa\n");
+}
+
+void	sort_five(t_stack **stack, t_stack **stack_b)
+{
 
 	push_b(stack, stack_b);
 	push_b(stack, stack_b);
 	sort_three(stack);
 	push_b(stack_b, stack);
-	list = search_list(*stack, 2);
+	/// on reprend demain !!!
 }
