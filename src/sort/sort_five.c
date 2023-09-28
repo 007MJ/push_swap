5d3cf713268ff6045513e3371884ae570d40db0f
007MJ <k.mijiza@gmail.com>
2023-09-27 21:15:24 -0400

    the five number  is sort

--- src/sort/sort_five.c
+++ src/sort/sort_five.c
@@ -6,7 +6,7 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 17:18:04 by mnshimiy          #+#    #+#             */
-/*   Updated: 2023/09/25 20:29:16 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2023/09/27 21:13:52 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ void	sort_number(t_stack **stack)
 	three = search_list(*stack, 3);
 	list = search_list(*stack, lstsize(*stack));
 	if ((*stack)->nb > list->nb)
-		rotate_a(stack, "ra");
-	if ((*stack)->nb > second->nb && (*stack)->nb < list->nb && (*stack)->nb > three->nb)
+		rotate_a(stack, "ra\n");
+	if ((*stack)->nb > second->nb && (*stack)->nb < list->nb && (*stack)->nb < three->nb)
 		swap_a_b(*stack, "sa\n");
 }
 
@@ -34,5 +34,7 @@ void	sort_five(t_stack **stack, t_stack **stack_b)
 	push_b(stack, stack_b);
 	sort_three(stack);
 	push_b(stack_b, stack);
-	/// on reprend demain !!!
+	sort_number(stack);
+	push_b(stack_b, stack);
+	sort_number(stack);
 }
