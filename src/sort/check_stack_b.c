984f3bfcbbb146c2995837152873015d11c4d5b5
007MJ <k.mijiza@gmail.com>
2023-11-02 01:26:54 -0400

    check si les function which_number fonctionne

--- src/sort/check_stack_b.c
+++ src/sort/check_stack_b.c
@@ -6,58 +6,58 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 11:42:31 by mnshimiy          #+#    #+#             */
-/*   Updated: 2023/10/31 23:31:18 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2023/11/02 01:23:27 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
 // void	check_stack_b(t_stack **stack, t_stack **stack_b, t_stack *list, int lenstack_a)
-{
-	t_stack	*second;
-	t_stack	*first;
+// {
+// 	t_stack	*second;
+// 	t_stack	*first;
 
 
-	int	stop;
-	(void)list;
-	stop = 0;
-	if (*stack_b == NULL)
-	{
-		printf("je suis dans null stack_b\n");
-		push_b(stack, stack_b);
-	}
-	else
-	{
-		second = search_list(*stack_b, 1);
-		first = search_list(*stack, 1);
-		while (stop == 0 && first != NULL && second != NULL)
-		{
-			first = search_list(*stack, 1);
-			second = search_list(*stack_b, 1);
-			if (first->nb > second->nb || lstsize(*stack_b) >= lenstack_a - 1)
-			{
-				printf("stop == 1 :first-> %d and second-> %d\n", first->nb, second->nb );
-				stop = 1;
-				push_b(stack, stack_b);
-			}
-			if (lstsize(*stack_b) < lenstack_a - 1 && stop != 1)
-			{
-				if (first->nb < second->nb )
-				{
-					if (lstsize(*stack_b) == 1)
-						push_b(stack, stack_b);
-					if( is_bigger(&first, stack_b) == 0)
-						push_b(stack, stack_b);
-					if(first->nb < second->nb )
-					{
-						printf("first-> %d and second-> %d\n", first->nb, second->nb );
-						rotate_a(stack_b, "--rb\n");
-					}
-				}
-			}
-		}
-	}
-}
+// 	int	stop;
+// 	(void)list;
+// 	stop = 0;
+// 	if (*stack_b == NULL)
+// 	{
+// 		printf("je suis dans null stack_b\n");
+// 		push_b(stack, stack_b);
+// 	}
+// 	else
+// 	{
+// 		second = search_list(*stack_b, 1);
+// 		first = search_list(*stack, 1);
+// 		while (stop == 0 && first != NULL && second != NULL)
+// 		{
+// 			first = search_list(*stack, 1);
+// 			second = search_list(*stack_b, 1);
+// 			if (first->nb > second->nb || lstsize(*stack_b) >= lenstack_a - 1)
+// 			{
+// 				printf("stop == 1 :first-> %d and second-> %d\n", first->nb, second->nb );
+// 				stop = 1;
+// 				push_b(stack, stack_b);
+// 			}
+// 			if (lstsize(*stack_b) < lenstack_a - 1 && stop != 1)
+// 			{
+// 				if (first->nb < second->nb )
+// 				{
+// 					if (lstsize(*stack_b) == 1)
+// 						push_b(stack, stack_b);
+// 					if( is_bigger(&first, stack_b) == 0)
+// 						push_b(stack, stack_b);
+// 					if(first->nb < second->nb )
+// 					{
+// 						printf("first-> %d and second-> %d\n", first->nb, second->nb );
+// 						rotate_a(stack_b, "--rb\n");
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 
 // void beforepush(t_stack ** stack)
 // {
@@ -85,13 +85,33 @@
 
 t_stack	*which_place(t_stack **stack_b, int i)
 {
-	int	number;
-	t_stack *list;
-	if (i > -1)
+	int		number;
+	t_stack	*list;
+
+	if (i > 0 && i < 4)
 	{
 		number = which_number(stack_b, i);
-		return (list = search_list(stack_b, number));
+		return (list = search_list(*stack_b, number));
 	}
+	else
+		return (NULL);
+}
+
+int	search_for_closer(int number, int smaller, int middle, int big)
+{
+	int	bgg;
+	int	mdd;
+	int	sml;
+
+	bgg = number - big;
+	mdd = number - middle;
+	sml = number - smaller;
+	if (bgg < mdd && bgg < sml && bgg > -1)
+		return (3);
+	else if (mdd < bgg && mdd < sml)
+		return (2);
+	else
+		return (1);
 }
 
 void	check_stack_b(t_stack **stack, t_stack **stack_b, t_stack *list, int lenstack_a)
@@ -100,6 +120,17 @@ void	check_stack_b(t_stack **stack, t_stack **stack_b, t_stack *list, int lensta
 	t_stack	*middle;
 	t_stack	*smaller;
 
-	big	= which_place(stack_b, 3);
-
+	big = NULL;
+	middle = NULL;
+	smaller = NULL;
+	(void)lenstack_a;
+	if (stack_b == NULL || lstsize(*stack_b) < 4)
+		push_b(stack, stack_b);
+	else
+	{
+		smaller = which_place(stack_b, 1);
+		middle = which_place(stack_b, 2);
+		big = which_place(stack_b, 3);
+		search_for_closer(list->nb, smaller->nb, middle->nb, big->nb);
+	}
 }
--- src/sort/which_number.c
+++ src/sort/which_number.c
@@ -6,7 +6,7 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 18:43:34 by mnshimiy          #+#    #+#             */
-/*   Updated: 2023/10/31 23:32:58 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2023/11/02 01:24:19 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,14 +38,92 @@ int	big(t_stack **stack_b)
 	return (-1);
 }
 
+int	is_closer_middle(int middle , t_stack **stack)
+{
+	t_stack	*curr;
+	t_stack	*list;
+	int		tmp;
+	int		i;
+	int		nb;
+
+	tmp = 90;
+	curr = *stack;
+	while (curr != NULL)
+	{
+		nb = middle - curr->nb;
+		if (tmp > nb && nb > -1)
+		{
+			tmp = nb;
+			list = curr;
+		}
+		curr = curr->next;
+	}
+	curr = *stack;
+	i = 1;
+	while (curr != NULL)
+	{
+		if (list->nb == curr->nb)
+			return (i);
+		i++;
+		curr = curr->next;
+	}
+	return (-1);
+}
+
+int	middle(t_stack **stack_b)
+{
+	t_stack	*curr;
+	int		middle;
+	int		divide;
+
+	divide = 1;
+	middle = 1;
+	curr = *stack_b;
+	while (curr != NULL)
+	{
+		middle += curr->nb;
+		divide++;
+		curr = curr->next;
+	}
+	middle = middle / divide;
+	if (middle % 2 != 0)
+		middle++;
+	return (is_closer_middle(middle, stack_b));
+}
+
+int	one (t_stack **stack)
+{
+	int	tmp;
+	int	i;
+	t_stack	*curr;
+
+	i = 1;
+	curr = *stack;
+	tmp = 90;
+	while (curr != NULL)
+	{
+		if (curr->nb < tmp)
+			tmp = curr->nb;
+		curr = curr->next;
+	}
+	curr = *stack;
+	while (curr != NULL)
+	{
+		if (curr->nb == tmp)
+			return (i);
+		curr = curr->next;
+		i++;
+	}
+	return (-1);
+}
+
 int which_number(t_stack **stack_b, int i)
 {
 	if (i == 3)
 		return ( big(stack_b));
 	else if (i == 2)
-		retrn (middle(stack_b,  i));
+		return (middle(stack_b));
 	else if (i == 1)
-		return (one(stack_b, i));
-	else
-		return (-1);
+		return (one(stack_b));
+	return (-1);
 }
