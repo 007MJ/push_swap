5e94c31d48a1d4d22626b671203b374e7ba26913
007MJ <k.mijiza@gmail.com>
2023-11-05 19:27:52 -0500

    work on the function

--- src/sort/go_place_number.c
+++ src/sort/go_place_number.c
@@ -6,43 +6,32 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 23:20:36 by mnshimiy          #+#    #+#             */
-/*   Updated: 2023/11/05 01:07:56 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2023/11/05 19:20:56 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-t_stack		*where_place(t_stack **next_b, t_stack **stack)
+int		*where_place(t_stack **next_b, t_stack **stack)
 {
-	t_stack	*curr;
-	t_stack	*tmp_list;
-	int		tmp;
-	int		save;
+	int	i;
+	t_stack *curr;
 
+	i = 0;
 	curr = *stack;
-	tmp = (*next_b)->nb - curr->nb;
-	save = tmp;
-	if (save < 0)
-		save *= -1;
-	if (tmp < save)
-	{
-		tmp = save;
-		tmp_list = curr;
-	}
-	curr = curr->next;
 	while (curr != NULL)
 	{
-		save = (*next_b)->nb - curr->nb;
-		if (save < 0)
-			save *= -1;
-		if (tmp < save)
-		{
-			tmp = save;
-			tmp_list = curr;
-		}
+		if ((*next_b)->nb > curr->nb)
+			return (i);
+		i++;
 		curr = curr->next;
 	}
-	return (tmp_list);
+	return (i);
+}
+
+void	find_move(int where, t_stack **stack, int lensize)
+{
+
 }
 
 void	go_place_number(t_stack **next_b, t_stack **stack)
@@ -51,6 +40,6 @@ void	go_place_number(t_stack **next_b, t_stack **stack)
 
 	list = NULL;
 	list = where_place(next_b, stack);
-	printf("closer to who %d\n", list->nb);
-	place_number(list, next_b, stack);
+	// printf("closer to who %d\n", list->nb);
+
 }
